f3c01333c41d3454626106e40a00b5084d4e9156
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-03-05 17:32:47 -0500

    update

--- src/components/layouts/header-top-line/index.js
+++ src/components/layouts/header-top-line/index.js
@@ -1,52 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import throttle from 'lodash.throttle'
-import cn from 'classnames'
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
 
-import Logo from './logo'
-import styles from './styles.module.scss'
-
-const HeaderTopLine = ({ className }) => {
-  const [hasScrolled, setHasScrolled] = useState(false)
-
-  useEffect(() => {
-    const handleScroll = throttle(() => {
-      const offset = 0
-      const { scrollTop } = document.documentElement
-      const scrolled = scrollTop > offset
-
-      if (hasScrolled !== scrolled) {
-        setHasScrolled(scrolled)
-      }
-    }, 200)
-
-    document.addEventListener('scroll', handleScroll)
-    return () => {
-      document.removeEventListener('scroll', handleScroll)
-    }
-  }, [hasScrolled])
-
+const HeaderTopLine = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div
-      className={cn(
-        className,
-        styles.wrapper,
-        hasScrolled ? styles.floatingNav : ''
-      )}
-    >
-      <div className={styles.leftBox}>
-        <Logo black={true} />
+    <>
+      <div className={styles.wrapper}>
+        <img src="/images/logo.png" className={styles.logo} />
+        <div className={styles.links}>
+          <a href="https://docs.f3manifesto.xyz/" className={styles.link} target="_blank" rel="noreferrer">
+            DOCUMENTATION
+          </a>
+          <a href="https://docs.f3manifesto.xyz/web3-fashion-manifesto/map" className={styles.link} target="_blank" rel="noreferrer">
+            Web3 Fashion manifesto
+          </a>
+          <a href="https://mirror.xyz/f3manifesto.eth" className={styles.link} target="_blank" rel="noreferrer">
+            Sub-thread weekly
+          </a>
+          <a href="https://themanifest.f3manifesto.xyz/" className={styles.link} target="_blank" rel="noreferrer">
+            The Manifest Gallery
+          </a>
+          <a href="https://web3cc0openlibrary.f3manifesto.xyz/" className={styles.link} target="_blank" rel="noreferrer">
+            Web3 CC0 Open Library
+          </a>
+        </div>
+        <button className={styles.mobileBtn} onClick={() => setShow(!show)}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </button>
       </div>
-    </div>
-  )
-}
+      {show && (
+        <div className={styles.mobileWrapper}>
+          <div className={styles.links}>
+            <a href="https://docs.f3manifesto.xyz" className={styles.link} target="_blank" rel="noreferrer">
+              DOCUMENTATION
+            </a>
+            <a href="https://docs.f3manifesto.xyz/web3-fashion-manifesto/map" className={styles.link} target="_blank" rel="noreferrer">
+              Web3 Fashion manifesto
+            </a>
+            <a href="https://mirror.xyz/f3manifesto.eth" className={styles.link} target="_blank" rel="noreferrer">
+              Sub-thread weekly
+            </a>
+            <a href="https://themanifest.f3manifesto.xyz/" className={styles.link} target="_blank" rel="noreferrer">
+              The Manifest Gallery
+            </a>
+            <a href="https://web3cc0openlibrary.f3manifesto.xyz/" className={styles.link} target="_blank" rel="noreferrer">
+              Web3 CC0 Open Library
+            </a>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-HeaderTopLine.propTypes = {
-  className: PropTypes.string
-}
-
-HeaderTopLine.defaultProps = {
-  className: ''
-}
-
-export default HeaderTopLine
+export default HeaderTopLine;
